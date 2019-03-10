daedecf04a38413b4cbff3fc3efefcc5ea133ce5
Justyna Jóźwik <just.jozwik@gmail.com>
2019-03-10 19:42:20 +0100

    Working app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'angular-random-name';
+export class AppComponent implements OnInit {
+  public randomName: string;
+
+  constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+    this.setName();
+  }
+
+  private setName(): void {
+    this.getNamesJSON().subscribe((data: string[]) => {
+      if (data) {
+        const idx = Math.floor(Math.random() * Math.floor(data.length));
+        this.randomName = data[idx];
+      }
+    });
+  }
+
+  private getNamesJSON(): Observable<any> {
+    return this.http.get("assets/names.json");
+  }
 }
